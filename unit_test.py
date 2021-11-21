14f93350548b010a3f045e7106b348baf2419183
John Crossland <John.Crossland@team.telstra.com>
2021-11-21 18:32:49 +1300

    Updated unit_test.py corrected test to equal 1
            data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 4)]
            result = sum(data)
            self.assertEqual(result, 1)
    
    Since previously Fraction(2, 5) so would equal 0.9 not 1.
    Also added decimal test and imported decimal module.

--- unit_test.py
+++ unit_test.py
@@ -2,7 +2,17 @@ import unittest
 
 from my_sum import sum
 
+import fractions
+
 from fractions import Fraction
+
+import decimal 
+
+from decimal import Decimal
+
+from decimal import getcontext
+
+getcontext().prec = 1
  
 class TestSum(unittest.TestCase):
     def test_list_int(self):
@@ -17,9 +27,17 @@ class TestSum(unittest.TestCase):
         """
         Test that it can sum a list of fractions
         """
-        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 4)]
         result = sum(data)
         self.assertEqual(result, 1)
- 
+    
+    def test_list_decimal(self):
+        """
+        Test that it can sum a list of decimals
+        """
+        data = [Decimal(1.1), Decimal(2.2), Decimal(3.2)]
+        result = sum(data)
+        self.assertEqual(result, 6.5)
+       
 if __name__ == '__main__':
     unittest.main()
